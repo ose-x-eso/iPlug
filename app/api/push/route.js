2b1275c6832df3f10069fe9f41c01a2785d850e9
Ose <josephconscience2@gmail.com>
2026-07-19 14:56:14 +0100

    fix: fixed server import typo

--- app/api/push/route.js
+++ app/api/push/route.js
@@ -1,5 +1,5 @@
 import { createClient } from '@/utils/supabase/server';
-import { NextResponse } from 'next/ NextResponse';
+import { NextResponse } from 'next/server';
 
 export async function POST(req) {
   try {
